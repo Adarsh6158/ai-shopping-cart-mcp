0280686aff4bea76d8596f0a4f0e26f72b658f16
adarshkumar <adarsh6158@gmail.com>
2026-03-29 22:19:36 +0530

    MCP server config changes

--- src/main/java/ai_shopping_cart_mcp/AiShoppingCartMcpApplication.java
+++ src/main/java/ai_shopping_cart_mcp/AiShoppingCartMcpApplication.java
@@ -1,6 +1,8 @@
 package ai_shopping_cart_mcp;
 
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,4 +17,10 @@ public class AiShoppingCartMcpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AiShoppingCartMcpApplication.class, args);
 	}
+	   @Bean
+    public ToolCallbackProvider shoppingCartTools(ShoppingCartMcpService shoppingCartMcpService) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(shoppingCartMcpService)
+                .build();
+    }
 }
